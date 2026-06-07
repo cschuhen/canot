6370d5972cf25809932eb70d92d20c3c00099567
Corey Schuhen <cschuhen@gmail.com>
2026-06-07 12:33:06 +1000

    Split out encoder to separate file.

--- apps/battery_monitor/src/main.rs
+++ apps/battery_monitor/src/main.rs
@@ -21,6 +21,8 @@ use rtic::app;
 pub mod application;
 mod bsp;
 pub mod consts;
+#[cfg(feature = "terminal")]
+pub mod encoder;
 pub mod error;
 pub mod graphical_elements;
 pub mod nvstore;
@@ -82,7 +84,7 @@ struct MainArgs {
 }
 
 #[cfg(feature = "terminal")]
-struct EncoderArgs(
+pub struct EncoderArgs(
     crate::bsp::InputPins,
     Sender<'static, CriticalSectionRawMutex, MainEvent, MAIN_EVENT_CAPACITY>,
     crate::bsp::BufferedCanErrorSender,
@@ -664,133 +666,9 @@ mod app {
         #[cfg(feature = "terminal")]
         {
             let mut guard = ENCODER_ARGS.get().await.lock().await;
-            run_encoder(&mut *guard).await;
+            crate::encoder::run_encoder(&mut *guard).await;
         }
         // Avoid unused variable warning
         let _cx = &cx;
-    }
-
-    #[cfg(feature = "terminal")]
-    async fn run_encoder(
-        //pins: &mut crate::bsp::InputPins,
-        //sender: &mut Sender<'static, MainEvent, MAIN_EVENT_CAPACITY>,
-        //error_sender: &mut crate::bsp::BufferedCanErrorSender,
-        args: &mut EncoderArgs,
-    ) {
-        //let mut delay = Delay {};
-        //delay.delay_ms(1000).await;
-
-        let EncoderArgs(pins, sender, error_sender) = args;
-        let [button, enc_a, enc_b] = pins;
-
-        // Debounce the inputs
-        let mut enc_a =
-            async_debounce::Debouncer::new(enc_a, embassy_time::Duration::from_micros(10));
-        let mut enc_b =
-            async_debounce::Debouncer::new(enc_b, embassy_time::Duration::from_micros(10));
-        let mut button =
-            async_debounce::Debouncer::new(button, embassy_time::Duration::from_micros(100));
-
-        #[derive(PartialEq)]
-        enum EncoderState {
-            A,
-            B,
-            Idle,
-        }
-
-        let mut press_time = Option::<embassy_time::Instant>::None;
-
-        let mut encoder_state: EncoderState = EncoderState::Idle;
-        loop {
-            use crate::application::HumanEvent;
-            use crate::application::MainEvent;
-            use embassy_futures::select::{select3, Either3};
-            use embedded_hal::digital::InputPin;
-            use embedded_hal_async::digital::Wait;
-            let res: Result<(), Error> = match select3(
-                enc_a.wait_for_any_edge(),
-                enc_b.wait_for_any_edge(),
-                button.wait_for_any_edge(),
-            )
-            .await
-            {
-                // For Encoder A and B. Just look for A,B raising edges or B,A raising edges. Any falling edge, resets state to idle.
-                Either3::First(_) => match enc_a.is_high() {
-                    Ok(true) => {
-                        if encoder_state == EncoderState::B {
-                            encoder_state = EncoderState::Idle;
-                            sender
-                                .send(MainEvent::HID(HumanEvent::EncoderAntiClockwise))
-                                .await;
-                            Ok(())
-                        } else {
-                            encoder_state = EncoderState::A;
-                            Ok(())
-                        }
-                    }
-                    Ok(false) => {
-                        encoder_state = EncoderState::Idle;
-                        Ok(())
-                    }
-                    Err(_) => {
-                        encoder_state = EncoderState::Idle;
-                        Ok(())
-                    }
-                },
-                Either3::Second(_) => match enc_b.is_high() {
-                    Ok(true) => {
-                        if encoder_state == EncoderState::A {
-                            encoder_state = EncoderState::Idle;
-                            sender
-                                .send(MainEvent::HID(HumanEvent::EncoderClockwise))
-                                .await;
-                            Ok(())
-                        } else {
-                            encoder_state = EncoderState::B;
-                            Ok(())
-                        }
-                    }
-                    Ok(false) => {
-                        encoder_state = EncoderState::Idle;
-                        Ok(())
-                    }
-                    Err(_) => {
-                        encoder_state = EncoderState::Idle;
-                        Ok(())
-                    }
-                },
-                Either3::Third(_) => match button.is_high() {
-                    Ok(true) => {
-                        press_time = Some(embassy_time::Instant::now());
-                        sender
-                            .send(MainEvent::HID(HumanEvent::EncoderButtonPressed))
-                            .await;
-                        Ok(())
-                    }
-                    Ok(false) => {
-                        if press_time.is_none() {
-                            // Spurious?
-                            Ok(())
-                        } else {
-                            let duration = embassy_time::Instant::now() - press_time.unwrap();
-                            sender
-                                .send(MainEvent::HID(HumanEvent::EncoderButtonReleased(duration)))
-                                .await;
-                            Ok(())
-                        }
-                    }
-                    Err(_) => {
-                        encoder_state = EncoderState::Idle;
-                        Ok(())
-                    }
-                },
-            };
-            match res {
-                Ok(_) => {}
-                Err(e) => {
-                    error_sender.send(&e);
-                }
-            }
-        }
     }
 }
